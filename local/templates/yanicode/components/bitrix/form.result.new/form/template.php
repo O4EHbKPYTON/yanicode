50799941638df7e55e143d8fafc2c80cdf42b4bd
O4EHbKPYTON <yan.matveyev@outlook.com>
2024-03-22 14:01:38 +0200

    fixed form result and now form works

--- local/templates/yanicode/components/bitrix/form.result.new/form/template.php
+++ local/templates/yanicode/components/bitrix/form.result.new/form/template.php
@@ -1,39 +1,56 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 
-if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? endif; ?>
+if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><?php endif; ?>
 
-<?php $count = 0; ?>
-<form action="" class="js-validated-form">
+<?= $arResult["FORM_NOTE"] ?>
 
-    <?php foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion): ?>
-        <?php if ($arQuestion["CAPTION"] == 'Название компании:' || $arQuestion["CAPTION"] == 'Опишите Вашу задачу:') : ?>
-            <div class="popup-feedback__input-cover">
-                <label for=""
-                       class="popup-feedback__input-label"><?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?></label>
-                <?= str_replace('class="inputtext" name="form_text_1" value="">', '<input type="text" class="popup-feedback__input">', '<input type="text" class="inputtext" name="form_text_1" value="">') ?>
-            </div>
-        <?php elseif ($arQuestion["CAPTION"] == 'Телефон' || $arQuestion["CAPTION"] == 'Email') : ?>
-            <?php if ($count == 0): ?>
-                <div class="popup-feedback__double-column">
-            <?php endif; ?>
-            <?php $count++; ?>
-            <div class="popup-feedback__input-cover">
-                <label for=""
-                       class="popup-feedback__input-label"><?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?></label>
-                <?= str_replace('class="inputtext" name="form_text_1" value="">', 'class="popup-feedback__input js-validated-field" data-validated_name="name">', '<input type="text" class="inputtext" name="form_text_1" value="">') ?>
-            </div>
-            <?php if ($count == 2): ?>
+<?php if ($arResult["isFormNote"] != "Y") {
+    ?>
+    <?= str_replace('<form', '<form class="js-validated-form"', $arResult["FORM_HEADER"]) ?>
+    <?php $count = 0; ?>
+    <?php
+    foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
+        if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'hidden') {
+            echo $arQuestion["HTML_CODE"];
+        } else {
+            ?>
+
+            <?php if ($arQuestion["CAPTION"] == 'Название компании:') : ?>
+                <div class="popup-feedback__input-cover">
+                    <label for=""
+                           class="popup-feedback__input-label"><?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?></label>
+                    <?= str_replace('class="inputtext"', 'class="popup-feedback__input"', $arQuestion["HTML_CODE"]) ?>
                 </div>
-            <?php endif; ?>
-        <?php else: ?>
-            <div class="popup-feedback__input-cover">
-                <label for=""
-                       class="popup-feedback__input-label"><?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?></label>
-                <?= str_replace('class="inputtext" name="form_text_1" value="">', 'class="popup-feedback__input js-validated-field" data-validated_name="name">', '<input type="text" class="inputtext" name="form_text_1" value="">') ?>
-            </div>
-        <?php endif; ?>
-    <?php endforeach; ?>
+                <?php elseif ($arQuestion["CAPTION"] == 'Опишите Вашу задачу:') : ?>
+                    <div class="popup-feedback__input-cover">
+                        <label for=""
+                               class="popup-feedback__input-label"><?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?></label>
+                        <?= str_replace('class="inputtextarea"', 'class="popup-feedback__textarea"', $arQuestion["HTML_CODE"]) ?>
+                    </div>
+                <?php elseif ($arQuestion["CAPTION"] == 'Телефон' || $arQuestion["CAPTION"] == 'Email') : ?>
+                    <?php if ($count == 0): ?>
+                        <div class="popup-feedback__double-column">
+                    <?php endif; ?>
+                    <?php $count++; ?>
+                    <div class="popup-feedback__input-cover">
+                        <label for=""
+                               class="popup-feedback__input-label"><?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?></label>
+                        <?= str_replace('class="inputtext"', 'class="popup-feedback__input js-validated-field" data-validated_name="name"', $arQuestion["HTML_CODE"]) ?>
+                    </div>
+                    <?php if ($count == 2): ?>
+                        </div>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <div class="popup-feedback__input-cover">
+                        <label for=""
+                               class="popup-feedback__input-label"><?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?></label>
+                        <?= str_replace('class="inputtext"', 'class="popup-feedback__input js-validated-field" data-validated_name="name"', $arQuestion["HTML_CODE"]) ?>
+                    </div>
+                <?php endif; ?>
+                <?php
+            }
+    } ?>
 
 
     <div class="popup-feedback__consent">
@@ -42,7 +59,8 @@ if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? 
             <label class="popup-feedback__consent-form" for="consent1">
                 <a href="./article-data-processing.html" target="_blank">
                     Нажимая кнопку «Отправить», я даю свое согласие на обработку моих персональных данных, в
-                    соответствии с Федеральным законом от 27.07.2006 года №152-ФЗ «О персональных данных», на условиях и
+                    соответствии с Федеральным законом от 27.07.2006 года №152-ФЗ «О персональных данных», на
+                    условиях и
                     для целей, определенных в Согласии на обработку персональных данных.
                 </a>
             </label>
@@ -51,7 +69,8 @@ if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? 
             <input class="popup-feedback__consent-input" id="consent2" type="checkbox" checked="checked"/>
             <label class="popup-feedback__consent-form" for="consent2">
                 <a href="./article-privacy-policy.html" target="_blank">
-                    Оставляя данные на Сайте, заполняя регистрационную форму, Вы соглашаетесь с настоящей Политикой
+                    Оставляя данные на Сайте, заполняя регистрационную форму, Вы соглашаетесь с настоящей
+                    Политикой
                     конфиденциальности.
                 </a>
             </label>
@@ -59,11 +78,15 @@ if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? 
     </div>
 
     <div class="popup-feedback__button-cover">
-        <input <?= (intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : ""); ?> type="submit"
-                                                                                          class="button button_modal-gold js-button-submit"
-                                                                                          name="web_form_submit"
-                                                                                          value="Отправить">
+        <input class="button button_modal-gold js-button-submit" <?= (intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : ""); ?>
+               type="submit"
+               name="web_form_submit"
+               value="Отправить">
 
     </div>
-</form>
 
+
+    <?= $arResult["FORM_FOOTER"] ?>
+    <?php
+}
+?>
--- local/templates/yanicode/components/bitrix/menu/yanicode_main_menu/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$MESS ['MENU_DOT_DEFAULT_NAME'] = "Кастомное главное меню по умолчанию";
-$MESS ['MENU_DOT_DEFAULT_DESC'] = "Кастомное главное меню по умолчанию";
-
